bed520e07bf2c896d038223705632a62e317531d
Aleksandr <ggwphp@yandex.ru>
2023-09-29 08:52:20 +0300

    test my JB IDE

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./Components/PagesElements/Header/Header";
+import {Article} from "./Components/PagesElements/Article/Article";
+import {Navbar} from "./Components/PagesElements/NavBar/Navbar";
+import {Footer} from "./Components/PagesElements/Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): React.ReactElement {
+    return (
+        <div className="App">
+            <Header/>
+            <Navbar/>
+            <Article/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/PagesElements/Article/Article.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Article:React.ElementType = () => {
+    return (<>
+    </>)
+}
